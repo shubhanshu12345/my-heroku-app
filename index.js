90a0a1222b5782f1a5bcb5587961b857bfb4d352
shubhanshu12345 <shubhanshusharma.co@gmail.com>
2021-05-10 22:11:24 +0530

    second commit

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded());
 app.use(express.static("assets"));
 
 app.get('/',function(req,res){
-    res.send("<h1> HELLO</h1>");
+    res.send("<h1>HELLO</h1>");
 })
 
 
-app.listen(8000, function (r, e) {
+app.listen(process.env.PORT, function (r, e) {
     if (e) {
       console.log(e);
     } else {
